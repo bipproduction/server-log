3f272bb5e28437f05e277e77de77a408515758ef
bipproduction <bip.production.js@gmail.com>
2023-10-04 21:01:49 +0800

    title: auto des: auto note:auto

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require('express');
 const { exec } = require('child_process');
 const app = express();
 app.use(express.static("public"))
-// app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 const port = config.server.port;
 let pm2Log = '';
--- xcoba.js
+++ xcoba.js
@@ -1,14 +1,7 @@
-const { execSync, exec } = require('child_process');
-const { fetch } = require('cross-fetch')
-let d;
-const log = exec('pm2 log hipmi_5005')
-log.stdout.on("data", (data) => {
-    d += data
-})
-setTimeout(() => {
-    log.kill("SIGHUP")
-    console.log(d)
-    fetch(`https://wa.wibudev.com/code?nom=6289697338821&text=${encodeURIComponent(d)}`).then((v) => {
-        console.log(v.status)
+const {fetch} = require('cross-fetch')
+fetch('https://log.wibudev.com/hipmi-log?pswd=1234', {
+    method: "POST",
+    body: JSON.stringify({
+        nama: "malik"
     })
-}, 2000);
+})
